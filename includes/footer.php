a20516b79bda87dd6f6ae37d1236766e5d401698
Tenzing Lhomi <147829240+lhomitenzing202@users.noreply.github.com>
2023-12-01 21:48:00 +0545

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Ktm Cap series Since 2022</b>
+    </div>
+    <strong>Copyright &copy; 2022 All rights reserved by Ktm Cap series</strong>
+</footer>
